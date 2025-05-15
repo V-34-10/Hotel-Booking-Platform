ccb4f4d998122f0b4c7cfc06ca07f0b15d5010e8
Vlad <vladandryschenko98@gmail.com>
2025-05-15 21:32:40 +0300

    dto: add BookingResponseDTO

---
+++ src/main/java/com/example/hotel_8/dto/BookingResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.hotel_8.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class BookingResponseDTO {
+  private Long id;
+  private String hotelName;
+  private String roomNumber;
+  private String guestName;
+  private LocalDate fromDate;
+  private LocalDate toDate;
+}
