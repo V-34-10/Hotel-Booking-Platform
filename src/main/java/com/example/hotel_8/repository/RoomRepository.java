3590b56fd3f0221c7032ccfa5e0fd3edb0cbacd9
Vlad <vladandryschenko98@gmail.com>
2025-05-15 21:27:40 +0300

    RoomRepository: add findByHotelId

---
+++ src/main/java/com/example/hotel_8/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.example.hotel_8.repository;
+
+import com.example.hotel_8.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findByHotelId(Long hotelId);
+}
