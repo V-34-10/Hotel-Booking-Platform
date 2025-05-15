863c356a50167d4e5696eb4fcc7f941b549fcb26
Vlad <vladandryschenko98@gmail.com>
2025-05-15 21:29:21 +0300

    RoomRepository/HotelService/HotelController: add getAllHotels, getHotelById, createHotel

---
+++ src/main/java/com/example/hotel_8/controller/HotelController.java
@@ -0,0 +1,32 @@
+package com.example.hotel_8.controller;
+
+import com.example.hotel_8.entity.Hotel;
+import com.example.hotel_8.service.HotelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/hotels")
+@RequiredArgsConstructor
+public class HotelController {
+
+  private final HotelService hotelService;
+
+  @GetMapping
+  public ResponseEntity<List<Hotel>> getAllHotels() {
+    return ResponseEntity.ok(hotelService.getAllHotels());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Hotel> getHotelById(@PathVariable Long id) {
+    return ResponseEntity.ok(hotelService.getHotelById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
+    return ResponseEntity.ok(hotelService.createHotel(hotel));
+  }
+}
---
+++ src/main/java/com/example/hotel_8/service/HotelService.java
@@ -0,0 +1,44 @@
+package com.example.hotel_8.service;
+
+import com.example.hotel_8.entity.Hotel;
+import com.example.hotel_8.repository.HotelRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class HotelService {
+    private static final Logger logger = LoggerFactory.getLogger(HotelService.class);
+    private final HotelRepository hotelRepository;
+
+    public List<Hotel> getAllHotels() {
+        logger.info("Fetching all hotels");
+        List<Hotel> hotels = hotelRepository.findAll();
+        logger.debug("Found {} hotels", hotels.size());
+        return hotels;
+    }
+
+    public Hotel getHotelById(Long id) {
+        logger.info("Fetching hotel with id: {}", id);
+        Hotel hotel = hotelRepository.findById(id)
+                .orElseThrow(() -> {
+                    logger.error("Hotel not found with id: {}", id);
+                    return new RuntimeException("Hotel not found with id: " + id);
+                });
+        logger.debug("Found hotel: {}", hotel.getName());
+        return hotel;
+    }
+
+    public Hotel createHotel(Hotel hotel) {
+        logger.info("Creating new hotel: {}", hotel.getName());
+        Hotel savedHotel = hotelRepository.save(hotel);
+        logger.debug("Created hotel with id: {}", savedHotel.getId());
+        return savedHotel;
+    }
+}
